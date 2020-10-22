cd48ac31a2252d476d9d7130b644100387fdc7d9
TOUTA-T <toutaby@aol.com>
2020-10-22 04:48:17 +0000

    ログインシステム実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  resources :users, only: [:new, :create,:show]
 end
