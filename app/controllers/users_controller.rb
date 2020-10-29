7b8e2579b57275a90212065c55a07895b8990127
TOUTA-T <toutaby@aol.com>
2020-10-29 07:57:31 +0000

    Fix users show page andcontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    # ユーザーページでお気に入り一覧を表示させる
     @posts = @user.posts
-    favorites = Favorite.where(user_id: current_user.id).pluck(:post_id) # ログイン中のユーザーのお気に入りのpost_idカラムを取得
-    @favorite_list = Post.find(favorites)     # postsテーブルから、お気に入り登録済みのレコードを取得
+    if @user.favorites.present?
+    favorites = Favorite.where(user_id: current_user.id).pluck(:post_id)
+    @favorite_list = Post.find(favorites)
+    else
+    @favorite_list = []
+    end
   end
 
   def edit
